c267595790828005bde72a9a809c607f8a7ea0ee
toshiemon18 <st12318@gmail.com>
2013-12-24 16:07:51 +0900

    とりあえず完成しました

--- build-tmp/source/uhr.java
+++ build-tmp/source/uhr.java
@@ -15,11 +15,6 @@ import java.io.IOException;
 public class uhr extends PApplet {
 
 //\u5186\u3092\u4f7f\u3063\u3066\u3050\u308b\u3050\u308b\u56de\u3057\u3066\u6642\u8a08\u30e9\u30a4\u30af\u306a\u3082\u306e
-//\u9032\u6357\u30c0\u30e1\u306a\u306e\u3067\u51ac\u4f11\u307f\u304b\u3089\u9811\u5f35\u308a\u307e\u3059(12/21\uff5e)
-//ToDo
-//-\u6642\u8a08\u56de\u308a\u306e\u52d5\u4f5c
-//-1\u5206\u3054\u3068\u306e\u79d2\u306e\u8aa4\u5dee\u306e\u4fee\u6b63
-//-\u524d\u306b\u63cf\u753b\u3057\u305f\u5186\u3092\u6d88\u53bb\u3059\u308b\u30a2\u30eb\u30b4\u30ea\u30ba\u30e0
 
 public void setup(){
 	size(displayWidth, displayHeight);
@@ -33,20 +28,24 @@ public void setup(){
 
 public void draw(){
 	background(0, 0, 0);
+	int pa = hour();
 	float sec, min, hou;//\u79d2\u3001\u5206\u3001\u6642\u9593
 	int s, m ,h;//\u30b3\u30f3\u30d0\u30fc\u30c8\u3057\u3066\u8868\u793a\u3059\u308b\u305f\u3081\u306e
 	float sRange = 300.0f;//\u79d2\u306e\u5186\u5468\u306e\u534a\u5f84
 	float mRange = 230.0f;//\u5206\u306e\u5186\u5468\u306e\u534a\u5f84
+	float hRange = 160.0f;
 	float hx = displayWidth / 2, hy = displayHeight / 2;//\u30c7\u30a3\u30b9\u30d7\u30ec\u30a4\u306e\u4e2d\u5fc3\u5ea7\u6a19
 	float sRad; //\u7b97\u51fa\u3057\u305f\u89d2\u5ea6\u3092\u683c\u7d0d\u3059\u308b(\u79d2\u5186\u7528)
 	float mRad;	//\u7b97\u51fa\u3057\u305f\u89d2\u5ea6\u3092\u683c\u7d0d\u3059\u308b(\u5206\u5186\u7528)
+	float hRad; //\u7b97\u51fa\u3057\u305f\u89d2\u5ea6\u3092\u683c\u7d0d\u3059\u308b(\u6642\u9593\u5186\u7528)
 
 	sec = second();
-	min = minute() + (sec/60.0f);
-	hou = hour() % 12.0f + (min/60.0f);
+	min = minute();
+	hou = hour() % 12.0f;
 
 	sRad = (360 * (sec - 15) / 60.0f) * -1;//\u79d2\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
-	mRad = (360 * (min - 15) /60.0f) * -1;//\u5206\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
+	mRad = (360 * (min - 15) / 60.0f) * -1;//\u5206\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
+	hRad = (360 * (hou - 15) / 60.0f) * -1;//\u6642\u9593\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
 
 	s = PApplet.parseInt(sec);//\u79d2\u306e\u5024\u3092float\u304b\u3089int\u3078\u30b3\u30f3\u30d0\u30fc\u30c8
 	m = PApplet.parseInt(min);//\u5206\u306e\u5024\u3092float\u304b\u3089int\u3078\u30b3\u30f3\u30d0\u30fc\u30c8
@@ -70,6 +69,29 @@ public void draw(){
 	text(m, sin(radians(mRad + 90)) * mRange + hx - 7, cos(radians(mRad + 90)) * mRange + hy + 8.5f);
 	noFill();
 
+	//\u6642\u9593\u5186\u306e\u8868\u73fe
+	fill(255, 255, 255);
+	ellipse(sin(radians(hRad + 90)) * hRange + hx, cos(radians(hRad + 90)) * hRange + hy, 20 * PI, 20 * PI);
+	noFill();
+	//\u6642\u9593\u306e\u5024\u3092\u5186\u306e\u4e2d\u306b\u8868\u793a
+	fill(0, 0, 0);
+	text(h, sin(radians(hRad + 90)) * hRange + hx - 7, cos(radians(hRad + 90)) * hRange + hy + 8.5f);
+	noFill();
+
+	fill(255, 255, 255);
+	ellipse(hx, hy, 30 * PI, 30 * PI);
+	noFill();
+	if(pa <= 12){
+		fill(0, 0, 0);
+		text("PM", hx - 13, hy + 8.5f);
+		noFill();
+	}
+
+	else{
+		fill(0, 0, 0);
+		text("AM", hx - 13, hy + 8.5f);
+		noFill();
+	}
 
 	println("Now time : "+h+":"+m+":"+s);
 }
--- uhr.pde
+++ uhr.pde
@@ -1,9 +1,4 @@
 //円を使ってぐるぐる回して時計ライクなもの
-//進捗ダメなので冬休みから頑張ります(12/21～)
-//ToDo
-//-時計回りの動作
-//-1分ごとの秒の誤差の修正
-//-前に描画した円を消去するアルゴリズム
 
 void setup(){
 	size(displayWidth, displayHeight);
@@ -17,20 +12,24 @@ void setup(){
 
 void draw(){
 	background(0, 0, 0);
+	int pa = hour();
 	float sec, min, hou;//秒、分、時間
 	int s, m ,h;//コンバートして表示するための
 	float sRange = 300.0;//秒の円周の半径
 	float mRange = 230.0;//分の円周の半径
+	float hRange = 160.0;
 	float hx = displayWidth / 2, hy = displayHeight / 2;//ディスプレイの中心座標
 	float sRad; //算出した角度を格納する(秒円用)
 	float mRad;	//算出した角度を格納する(分円用)
+	float hRad; //算出した角度を格納する(時間円用)
 
 	sec = second();
-	min = minute() + (sec/60.0);
-	hou = hour() % 12.0 + (min/60.0);
+	min = minute();
+	hou = hour() % 12.0;
 
 	sRad = (360 * (sec - 15) / 60.0) * -1;//秒の角度導出式
-	mRad = (360 * (min - 15) /60.0) * -1;//分の角度導出式
+	mRad = (360 * (min - 15) / 60.0) * -1;//分の角度導出式
+	hRad = (360 * (hou - 15) / 60.0) * -1;//時間の角度導出式
 
 	s = int(sec);//秒の値をfloatからintへコンバート
 	m = int(min);//分の値をfloatからintへコンバート
@@ -54,6 +53,29 @@ void draw(){
 	text(m, sin(radians(mRad + 90)) * mRange + hx - 7, cos(radians(mRad + 90)) * mRange + hy + 8.5);
 	noFill();
 
+	//時間円の表現
+	fill(255, 255, 255);
+	ellipse(sin(radians(hRad + 90)) * hRange + hx, cos(radians(hRad + 90)) * hRange + hy, 20 * PI, 20 * PI);
+	noFill();
+	//時間の値を円の中に表示
+	fill(0, 0, 0);
+	text(h, sin(radians(hRad + 90)) * hRange + hx - 7, cos(radians(hRad + 90)) * hRange + hy + 8.5);
+	noFill();
+
+	fill(255, 255, 255);
+	ellipse(hx, hy, 30 * PI, 30 * PI);
+	noFill();
+	if(pa <= 12){
+		fill(0, 0, 0);
+		text("PM", hx - 13, hy + 8.5);
+		noFill();
+	}
+
+	else{
+		fill(0, 0, 0);
+		text("AM", hx - 13, hy + 8.5);
+		noFill();
+	}
 
 	println("Now time : "+h+":"+m+":"+s);
 }
