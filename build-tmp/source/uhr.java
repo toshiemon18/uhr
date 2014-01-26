b93ddca5029af28c6b7f296b6137f04c4e85a307
toshiemon18 <st12318@gmail.com>
2014-01-26 21:46:28 +0900

    きれいなコードの書き方教えてください。

--- build-tmp/source/uhr.java
+++ build-tmp/source/uhr.java
@@ -14,86 +14,112 @@ import java.io.IOException;
 
 public class uhr extends PApplet {
 
-//\u5186\u3092\u4f7f\u3063\u3066\u3050\u308b\u3050\u308b\u56de\u3057\u3066\u6642\u8a08\u30e9\u30a4\u30af\u306a\u3082\u306e
+//\u5186\u3092\u4f7f\u3063\u3066\u3050\u308b\u3050\u308b\u56de\u3057\u3066\u6642\u8a08\u3063\u307d\u3044\u3082\u306e
+int disp = 600;
+float sRange = 250.0f;//\u79d2\u306e\u5186\u5468\u306e\u534a\u5f84
+float mRange = 180.0f;//\u5206\u306e\u5186\u5468\u306e\u534a\u5f84
+float hRange = 110.0f;
+float hx = disp / 2.0f, hy = disp / 2.0f;
 
 public void setup(){
-	size(displayWidth, displayHeight);
+	size(disp, disp);
 	colorMode(RGB);
 	background(0, 0, 0);
-	smooth();
-	PFont uhr = loadFont("AgencyFB-Reg-18.vlw");
-  	textFont(uhr, 21);
+	PFont uhr = loadFont("AgencyFB-Reg-25.vlw");
+  	textFont(uhr, 23);
   	stroke(255, 255, 255);
+  	smooth();
 }
 
 public void draw(){
 	background(0, 0, 0);
 	int pa = hour();
 	float sec, min, hou;//\u79d2\u3001\u5206\u3001\u6642\u9593
-	int s, m ,h;//\u30b3\u30f3\u30d0\u30fc\u30c8\u3057\u3066\u8868\u793a\u3059\u308b\u305f\u3081\u306e
-	float sRange = 300.0f;//\u79d2\u306e\u5186\u5468\u306e\u534a\u5f84
-	float mRange = 230.0f;//\u5206\u306e\u5186\u5468\u306e\u534a\u5f84
-	float hRange = 160.0f;
-	float hx = displayWidth / 2, hy = displayHeight / 2;//\u30c7\u30a3\u30b9\u30d7\u30ec\u30a4\u306e\u4e2d\u5fc3\u5ea7\u6a19
-	float sRad; //\u7b97\u51fa\u3057\u305f\u89d2\u5ea6\u3092\u683c\u7d0d\u3059\u308b(\u79d2\u5186\u7528)
-	float mRad;	//\u7b97\u51fa\u3057\u305f\u89d2\u5ea6\u3092\u683c\u7d0d\u3059\u308b(\u5206\u5186\u7528)
-	float hRad; //\u7b97\u51fa\u3057\u305f\u89d2\u5ea6\u3092\u683c\u7d0d\u3059\u308b(\u6642\u9593\u5186\u7528)
+	int s, m ,h;//\u30b3\u30f3\u30d0\u30fc\u30c8\u3057\u3066\u8868\u793a\u3059\u308b\u305f\u3081\u306e	
+	float srad, mrad, hrad; //\u89d2\u5ea6\u683c\u7d0d\u7528
+	String s_s, m_s, h_s;
 
+	//\u30c6\u30ad\u30b9\u30c8\u5909\u63db
+	//\u79d2\u6570\u304c1\u6841\u306e\u3068\u304d\u306f\u5148\u982d\u306b0\u3092\u3064\u3051\u308b
+	if(second() < 10)
+		s_s = "0" + str(second());
+	else
+		s_s = str(second());
+
+	//\u5206\u304c1\u6841\u306e\u3068\u304d\u306f\u5148\u982d\u306b0\u3092\u3064\u3051\u308b
+	if(minute() < 10)
+		m_s = "0" + str(minute());
+	else
+		m_s = str(minute());
+
+	//\u6642\u9593\u304c1\u6841\u306e\u6642\u306f\u5148\u982d\u306b0\u3092\u3064\u3051\u308b
+	if(hour() < 10)
+		h_s = "0" + str(hour());
+	else
+		h_s = str(hour());
+
+	//\u6642\u9593\u306e\u53d6\u5f97
 	sec = second();
 	min = minute();
 	hou = hour() % 12.0f;
 
-	sRad = (360 * (sec - 15) / 60.0f) * -1;//\u79d2\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
-	mRad = (360 * (min - 15) / 60.0f) * -1;//\u5206\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
-	hRad = (360 * (hou - 15) / 60.0f) * -1;//\u6642\u9593\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
+	//\u5186\u306e\u8868\u793a\u4f4d\u7f6e\u3092\u6c7a\u5b9a\u3059\u308b\u89d2\u5ea6\u306e\u5c0e\u51fa
+	srad = (360 * (sec - 15) / 60.0f) * -1;//\u79d2\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
+	mrad = (360 * (min - 15) / 60.0f) * -1;//\u5206\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
+	hrad = (360 * (hou - 15) / 60.0f) * -1;//\u6642\u9593\u306e\u89d2\u5ea6\u5c0e\u51fa\u5f0f
 
 	s = PApplet.parseInt(sec);//\u79d2\u306e\u5024\u3092float\u304b\u3089int\u3078\u30b3\u30f3\u30d0\u30fc\u30c8
 	m = PApplet.parseInt(min);//\u5206\u306e\u5024\u3092float\u304b\u3089int\u3078\u30b3\u30f3\u30d0\u30fc\u30c8
 	h = PApplet.parseInt(hou);//\u6642\u9593\u306e\u5024\u3092float\u304b\u3089int\u3078\u30b3\u30f3\u30d0\u30fc\u30c8
 
+	//\u30a6\u30a3\u30f3\u30c9\u30a6\u306e\u89d2\u306b\u6642\u9593\u3068\u30d7\u30ed\u30b0\u30e9\u30e0\u540d\u3072\u3087\u3046\u3058\u3059\u308b
+	fill(255, 255, 255);
+	text("Uhr", 10, 20);
+	text(h_s + " : " + m_s + " : " + s_s, 10, 40);
+	noFill();
+
 	//\u79d2\u5186\u306e\u8868\u73fe
 	fill(255, 255, 255);
-	ellipse(sin(radians(sRad + 90)) * sRange + hx, cos(radians(sRad + 90)) * sRange + hy, 20 * PI, 20 * PI);
+	ellipse(sin(radians(srad + 90)) * sRange + hx, cos(radians(srad + 90)) * sRange + hy, 60, 60);
 	noFill();
 	//\u79d2\u306e\u5024\u3092\u5186\u306e\u4e2d\u306b\u8868\u793a	
 	fill(0, 0, 0);
-	text(s, sin(radians(sRad + 90)) * sRange + hx - 7, cos(radians(sRad + 90)) * sRange + hy + 8.5f);
+	text(s_s + "s", sin(radians(srad + 90)) * sRange + hx - 7.5f, cos(radians(srad + 90)) * sRange + hy + 8.5f);
 	noFill();
 	
 	//\u5206\u5186\u306e\u8868\u73fe
 	fill(255, 255, 255);
-	ellipse(sin(radians(mRad + 90)) * mRange + hx, cos(radians(mRad + 90)) * mRange + hy, 20 * PI, 20 * PI);
+	ellipse(sin(radians(mrad + 90)) * mRange + hx, cos(radians(mrad + 90)) * mRange + hy, 60, 60);
 	noFill();
 	//\u5206\u306e\u5024\u3092\u5186\u306e\u4e2d\u306b\u8868\u793a
 	fill(0, 0, 0);
-	text(m, sin(radians(mRad + 90)) * mRange + hx - 7, cos(radians(mRad + 90)) * mRange + hy + 8.5f);
+	text(m_s + "m", sin(radians(mrad + 90)) * mRange + hx - 7.5f, cos(radians(mrad + 90)) * mRange + hy + 8.5f);
 	noFill();
 
 	//\u6642\u9593\u5186\u306e\u8868\u73fe
 	fill(255, 255, 255);
-	ellipse(sin(radians(hRad + 90)) * hRange + hx, cos(radians(hRad + 90)) * hRange + hy, 20 * PI, 20 * PI);
+	ellipse(sin(radians(hrad + 90)) * hRange + hx, cos(radians(hrad + 90)) * hRange + hy, 60, 60);
 	noFill();
 	//\u6642\u9593\u306e\u5024\u3092\u5186\u306e\u4e2d\u306b\u8868\u793a
 	fill(0, 0, 0);
-	text(h, sin(radians(hRad + 90)) * hRange + hx - 7, cos(radians(hRad + 90)) * hRange + hy + 8.5f);
+	text(h_s + "h", sin(radians(hrad + 90)) * hRange + hx - 7.5f, cos(radians(hrad + 90)) * hRange + hy + 8.5f);
 	noFill();
 
+	//AM\u30fbPM\u3092\u8868\u793a\u3059\u308b\u5186\u3092\u63cf\u753b
 	fill(255, 255, 255);
-	ellipse(hx, hy, 30 * PI, 30 * PI);
+	ellipse(hx, hy, 30 * 3, 30 * 3);
 	noFill();
 	if(pa <= 12){
 		fill(0, 0, 0);
-		text("PM", hx - 13, hy + 8.5f);
+		text(" PM", hx - 14, hy + 9);
 		noFill();
 	}
-
 	else{
 		fill(0, 0, 0);
-		text("AM", hx - 13, hy + 8.5f);
+		text(" AM", hx - 14, hy + 9);
 		noFill();
 	}
-
-	println("Now time : "+h+":"+m+":"+s);
+	//println("Now time : "+h+":"+m+":"+s);
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "uhr" };
--- uhr.pde
+++ uhr.pde
@@ -1,81 +1,107 @@
-//円を使ってぐるぐる回して時計ライクなもの
+//円を使ってぐるぐる回して時計っぽいもの
+int disp = 600;
+float sRange = 250.0;//秒の円周の半径
+float mRange = 180.0;//分の円周の半径
+float hRange = 110.0;
+float hx = disp / 2.0, hy = disp / 2.0;
 
 void setup(){
-	size(displayWidth, displayHeight);
+	size(disp, disp);
 	colorMode(RGB);
 	background(0, 0, 0);
-	smooth();
-	PFont uhr = loadFont("AgencyFB-Reg-18.vlw");
-  	textFont(uhr, 21);
+	PFont uhr = loadFont("AgencyFB-Reg-25.vlw");
+  	textFont(uhr, 23);
   	stroke(255, 255, 255);
+  	smooth();
 }
 
 void draw(){
 	background(0, 0, 0);
 	int pa = hour();
 	float sec, min, hou;//秒、分、時間
-	int s, m ,h;//コンバートして表示するための
-	float sRange = 300.0;//秒の円周の半径
-	float mRange = 230.0;//分の円周の半径
-	float hRange = 160.0;
-	float hx = displayWidth / 2, hy = displayHeight / 2;//ディスプレイの中心座標
-	float sRad; //算出した角度を格納する(秒円用)
-	float mRad;	//算出した角度を格納する(分円用)
-	float hRad; //算出した角度を格納する(時間円用)
+	int s, m ,h;//コンバートして表示するための	
+	float srad, mrad, hrad; //角度格納用
+	String s_s, m_s, h_s;
 
+	//テキスト変換
+	//秒数が1桁のときは先頭に0をつける
+	if(second() < 10)
+		s_s = "0" + str(second());
+	else
+		s_s = str(second());
+
+	//分が1桁のときは先頭に0をつける
+	if(minute() < 10)
+		m_s = "0" + str(minute());
+	else
+		m_s = str(minute());
+
+	//時間が1桁の時は先頭に0をつける
+	if(hour() < 10)
+		h_s = "0" + str(hour());
+	else
+		h_s = str(hour());
+
+	//時間の取得
 	sec = second();
 	min = minute();
 	hou = hour() % 12.0;
 
-	sRad = (360 * (sec - 15) / 60.0) * -1;//秒の角度導出式
-	mRad = (360 * (min - 15) / 60.0) * -1;//分の角度導出式
-	hRad = (360 * (hou - 15) / 60.0) * -1;//時間の角度導出式
+	//円の表示位置を決定する角度の導出
+	srad = (360 * (sec - 15) / 60.0) * -1;//秒の角度導出式
+	mrad = (360 * (min - 15) / 60.0) * -1;//分の角度導出式
+	hrad = (360 * (hou - 15) / 60.0) * -1;//時間の角度導出式
 
 	s = int(sec);//秒の値をfloatからintへコンバート
 	m = int(min);//分の値をfloatからintへコンバート
 	h = int(hou);//時間の値をfloatからintへコンバート
 
+	//ウィンドウの角に時間とプログラム名ひょうじする
+	fill(255, 255, 255);
+	text("Uhr", 10, 20);
+	text(h_s + " : " + m_s + " : " + s_s, 10, 40);
+	noFill();
+
 	//秒円の表現
 	fill(255, 255, 255);
-	ellipse(sin(radians(sRad + 90)) * sRange + hx, cos(radians(sRad + 90)) * sRange + hy, 20 * PI, 20 * PI);
+	ellipse(sin(radians(srad + 90)) * sRange + hx, cos(radians(srad + 90)) * sRange + hy, 60, 60);
 	noFill();
 	//秒の値を円の中に表示	
 	fill(0, 0, 0);
-	text(s, sin(radians(sRad + 90)) * sRange + hx - 7, cos(radians(sRad + 90)) * sRange + hy + 8.5);
+	text(s_s + "s", sin(radians(srad + 90)) * sRange + hx - 7.5, cos(radians(srad + 90)) * sRange + hy + 8.5);
 	noFill();
 	
 	//分円の表現
 	fill(255, 255, 255);
-	ellipse(sin(radians(mRad + 90)) * mRange + hx, cos(radians(mRad + 90)) * mRange + hy, 20 * PI, 20 * PI);
+	ellipse(sin(radians(mrad + 90)) * mRange + hx, cos(radians(mrad + 90)) * mRange + hy, 60, 60);
 	noFill();
 	//分の値を円の中に表示
 	fill(0, 0, 0);
-	text(m, sin(radians(mRad + 90)) * mRange + hx - 7, cos(radians(mRad + 90)) * mRange + hy + 8.5);
+	text(m_s + "m", sin(radians(mrad + 90)) * mRange + hx - 7.5, cos(radians(mrad + 90)) * mRange + hy + 8.5);
 	noFill();
 
 	//時間円の表現
 	fill(255, 255, 255);
-	ellipse(sin(radians(hRad + 90)) * hRange + hx, cos(radians(hRad + 90)) * hRange + hy, 20 * PI, 20 * PI);
+	ellipse(sin(radians(hrad + 90)) * hRange + hx, cos(radians(hrad + 90)) * hRange + hy, 60, 60);
 	noFill();
 	//時間の値を円の中に表示
 	fill(0, 0, 0);
-	text(h, sin(radians(hRad + 90)) * hRange + hx - 7, cos(radians(hRad + 90)) * hRange + hy + 8.5);
+	text(h_s + "h", sin(radians(hrad + 90)) * hRange + hx - 7.5, cos(radians(hrad + 90)) * hRange + hy + 8.5);
 	noFill();
 
+	//AM・PMを表示する円を描画
 	fill(255, 255, 255);
-	ellipse(hx, hy, 30 * PI, 30 * PI);
+	ellipse(hx, hy, 30 * 3, 30 * 3);
 	noFill();
 	if(pa <= 12){
 		fill(0, 0, 0);
-		text("PM", hx - 13, hy + 8.5);
+		text(" PM", hx - 14, hy + 9);
 		noFill();
 	}
-
 	else{
 		fill(0, 0, 0);
-		text("AM", hx - 13, hy + 8.5);
+		text(" AM", hx - 14, hy + 9);
 		noFill();
 	}
-
-	println("Now time : "+h+":"+m+":"+s);
+	//println("Now time : "+h+":"+m+":"+s);
 }
